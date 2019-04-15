7f0e980be67f09a871cd58526f70bddf087c1d8c
v_zwzou <v_zwzou@tencent.com>
2019-04-15 14:40:19 +0800

    delete Extra files

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 var express = require('express');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var apiRouter = require('./routes/api')
 
 var server = express();
 
 server.use('/', indexRouter);
-server.use('/users', usersRouter);
 server.use('/api', apiRouter);
 
 module.exports = server;
